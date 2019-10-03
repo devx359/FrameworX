8f52de1ef74175c869fee03a2a9b9f531f94a538
Haldar <haldard@ap.teo.earth>
2019-10-03 15:28:14 +0530

    new changes for test

---
+++ src/test/java/TestCases/FiveG/Test2.java
@@ -0,0 +1,47 @@
+package TestCases.FiveG;
+
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+
+import java.util.concurrent.TimeUnit;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+
+public class Test2 {
+	
+	@Test
+	public void meth()
+	{
+		RestAssured.baseURI="https://reqres.in/api";
+		
+		JSONObject req= new JSONObject();
+		req.put("name", "morpheus");
+		req.put("job","leader");
+		
+		System.out.println(req);
+		
+		//post
+		Response res = given().body(req).
+					    when().post("/users").
+					    then().extract().response();
+					    		
+		System.out.println(res.body().asString());
+		System.out.println("Time taken "+res.getTimeIn(TimeUnit.MILLISECONDS));
+		System.out.println("Status code "+res.getStatusLine());
+		
+		//get
+		
+		res= given().params("page",2).when().get("/users").then().extract().response();
+		System.out.print("GET response ");
+		System.out.print(res.body().asString());
+		
+	
+	}
+
+}
